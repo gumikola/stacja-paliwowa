1afe5bd566479096e56d54e80bfeb6d54515cb85
Mikołaj Guz <noqell@gmail.com>
2018-11-18 17:03:05 +0100

    Fill DataBaseApi class

--- DataBase/DataBaseApi.cpp
+++ DataBase/DataBaseApi.cpp
@@ -1,8 +1,31 @@
 #include "DataBaseApi.h"
 
-DataBaseApi::DataBaseApi() {}
+DataBaseApi::DataBaseApi()
+    : driverName("QSQLITE"),
+      pathDatabase("/home/mguz/workspace/stacja-paliwowasqlite.db") {
+  driverDatabase = QSqlDatabase::addDatabase(driverName);
+  driverDatabase.setDatabaseName(pathDatabase);
 
-QMap<QString, QMap<QString, uint32_t> > DataBaseApi::getTanksFillLevel() {
-  QMap<QString, QMap<QString, uint32_t> > b;
-  return b;
+  // Checking DB connection
+  if (driverDatabase.open() == false) {
+    qDebug() << "Error: connection with database fail";
+  }
+}
+
+QMap<QString, QMap<QString, uint32_t> > DataBaseApi::getTanksFillLevel(void) {
+  QMap<QString, QMap<QString, uint32_t> > data;
+  QSqlQuery query;
+
+  query.prepare("SELECT * FROM Magazyny ");
+
+  if (query.exec()) {
+    QMap<QString, uint> tmp;
+    while (query.next()) {
+      tmp.insert(query.value("Typ_paliwa_Nazwa").toString() + " " + query.value("ID").toString(),
+                 query.value("Zawartosc").toUInt());
+      data.insert(query.value("Miejsce").toString(), tmp);
+    }
+  }
+
+  return data;
 }
--- DataBase/DataBaseApi.h
+++ DataBase/DataBaseApi.h
@@ -2,12 +2,20 @@
 #define DATABASEAPI_H
 #include <QDate>
 #include <QMap>
+#include <QtSql/QtSql>
 
 class DataBaseApi {
+private:
+    QString driverName;
+    QString pathDatabase;
+
+    QSqlDatabase driverDatabase;
+
  public:
   DataBaseApi();
 
-  QMap<QString, QMap<QString, uint32_t>> getTanksFillLevel();
+  // TODO: Get all order from choosed date
+  QMap<QString, QMap<QString, uint32_t>> getTanksFillLevel(void);
 };
 
 #endif  // DATABASEAPI_H
